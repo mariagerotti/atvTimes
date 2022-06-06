024cb1cb21574ef383f58f4220e4c2f111cffe16
Victor <99228756+VictorLDS@users.noreply.github.com>
2022-06-06 09:19:08 -0300

    Add files via upload

---
+++ attlistajt/src/views/TimesView.vue
@@ -0,0 +1,84 @@
+<template>
+  <main>
+    <div class="container">
+      <div class="title">
+        <h2>Gerenciamento de Times</h2>
+      </div>
+      <div class="form-input">
+        <input type="text" />
+        <button>Salvar</button>
+      </div>
+      <div class="list-times">
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nome</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Time 1</td>
+              <td>??</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Time 2</td>
+              <td>??</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>Time 3</td>
+              <td>??</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </main>
+</template>
+<style>
+.title {
+  display: flex;
+  justify-content: center;
+  margin-top: 30px;
+}
+.form-input {
+  margin-top: 10px;
+  display: flex;
+  justify-content: center;
+}
+.form-input input {
+  width: 60%;
+  height: 40px;
+  border: 1px solid rgb(147, 147, 147);
+  border-radius: 10px;
+  padding: 0 10px;
+}
+.form-input button {
+  margin-left: 1px;
+  width: 20%;
+  height: 40px;
+  border: 1px solid rgb(211, 211, 211);
+  border-radius: 10px;
+  background-color: rgb(227, 170, 0);
+  color: rgb(255, 255, 255);
+  font-weight: bold;
+  cursor: pointer;
+}
+.list-times{
+  display: flex;
+  justify-content: center;
+}
+.list-times table{
+  width: 80%;
+  margin: 0 auto;
+  border-collapse: collapse;
+}
+table tr td{
+  border: 1px solid rgb(211, 211, 211);
+  padding: 10px;
+}
+</style>
