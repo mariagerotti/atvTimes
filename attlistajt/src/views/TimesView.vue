121746a4a08e2a25915fd6933eb9fbd82694fb05
mariagerotti <mariaeduarda.gerotti@gmail.com>
2022-07-14 14:43:56 -0300

    upload

--- attlistajt/src/views/TimesView.vue
+++ attlistajt/src/views/TimesView.vue
@@ -1,36 +1,38 @@
 <script>
-import axios from "axios";
-export default {
-  data() {
-    return {
-      times: [],
-      novo_time: "",
-    };
-  },
-  async created() {
-    const times = await axios.get("http://localhost:4000/times/");
-    this.times  = times.data;
-  },
-  methods: {
-    async salvar() {
-     if (this.times.id) {
-       const time_alterado = await axios.put(`http://localhost:4000/times/${this.time.id}`,
-       this.time
-       );
-     } else {
-        const time_criado = await axios.post(
-        "http://localhost:4000/times/", 
-        this.time);
-     }
-      this.times.push(time_criado.data);
+  import axios from "axios";
+  export default {
+    data() {
+      return {
+        times: [],
+        time: {},
+      };
     },
-    async excluir(time){
-      await axios.delete(`http://localhost:4000/times/${time.id}`);
-      const indice = this.times.indexOf(time);
-      this.times.splice(indice, 1);
+    async created() {
+      const times = await axios.get("http://localhost:4000/times/");
+      this.times = times.data;
     },
-  },
-};
+    methods: {
+      async salvar() {
+        if (this.times.id) {
+          const time_alterado = await axios.put(
+            `http://localhost:4000/times/${this.time.id}`,
+            this.time
+          );
+        } else {
+          const time_criado = await axios.post(
+            "http://localhost:4000/times/",
+            this.time
+          );
+          this.times.push(time_criado.data);
+        }
+      },
+      async excluir(time) {
+        await axios.delete(`http://localhost:4000/times/${time.id}`);
+        const indice = this.times.indexOf(time);
+        this.times.splice(indice, 1);
+      },
+    },
+  };
 </script>
 <template>
   <main>
@@ -39,7 +41,7 @@ export default {
         <h2>Gerenciamento de Times</h2>
       </div>
       <div class="form-input">
-        <input type="text" v-model="novo_time" />
+        <input type="text" v-model="time.nome" />
         <button @click="salvar">Salvar</button>
       </div>
       <div class="list-times">
@@ -69,44 +71,44 @@ export default {
   </main>
 </template>
 <style>
-.title {
-  display: flex;
-  justify-content: center;
-  margin-top: 30px;
-}
-.form-input {
-  margin-top: 10px;
-  display: flex;
-  justify-content: center;
-}
-.form-input input {
-  width: 60%;
-  height: 40px;
-  border: 1px solid rgb(147, 147, 147);
-  border-radius: 10px;
-  padding: 0 10px;
-}
-.form-input button {
-  margin-left: 1px;
-  width: 20%;
-  height: 40px;
-  border: 1px solid rgb(211, 211, 211);
-  border-radius: 10px;
-  color: rgb(255, 255, 255);
-  font-weight: bold;
-  cursor: pointer;
-}
-.list-times {
-  display: flex;
-  justify-content: center;
-}
-.list-times table {
-  width: 80%;
-  margin: 0 auto;
-  border-collapse: collapse;
-}
-table tr td {
-  border: 1px solid rgb(211, 211, 211);
-  padding: 10px;
-}
+  .title {
+    display: flex;
+    justify-content: center;
+    margin-top: 30px;
+  }
+  .form-input {
+    margin-top: 10px;
+    display: flex;
+    justify-content: center;
+  }
+  .form-input input {
+    width: 60%;
+    height: 40px;
+    border: 1px solid rgb(147, 147, 147);
+    border-radius: 10px;
+    padding: 0 10px;
+  }
+  .form-input button {
+    margin-left: 1px;
+    width: 20%;
+    height: 40px;
+    border: 1px solid rgb(211, 211, 211);
+    border-radius: 10px;
+    color: rgb(255, 255, 255);
+    font-weight: bold;
+    cursor: pointer;
+  }
+  .list-times {
+    display: flex;
+    justify-content: center;
+  }
+  .list-times table {
+    width: 80%;
+    margin: 0 auto;
+    border-collapse: collapse;
+  }
+  table tr td {
+    border: 1px solid rgb(211, 211, 211);
+    padding: 10px;
+  }
 </style>
